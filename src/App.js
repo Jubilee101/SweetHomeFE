40bc85095eefa91e351eda9f019b4d0a17f8573c
Jubilee101 <haoranz4@andrew.cmu.edu>
2022-07-31 20:01:09 -0400

    fix bugs, refactor codes

--- src/App.js
+++ src/App.js
@@ -41,14 +41,14 @@ class App extends Component {
   }
 
   renderContent = () => {
-    // if (!this.state.authed) {
-      // return <LoginPage handleLoginSuccess={this.handleLoginSuccess} />;
-    // }
+    if (!this.state.authed) {
+      return <LoginPageAlter handleLoginSuccess={this.handleLoginSuccess}/>
+    }
 
-    // if (this.state.asManager) {
-      // return <ManagerHomePage />;
-    // }
-
+    if (this.state.asManager) {
+      return <ManagerHomePage />;
+    }
+    
     return <ResidentHomePage />;
   };
 
--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -19,10 +19,10 @@ class Dashboard extends React.Component {
     render() {
         return (
             <Row className="dashBorad">
-                <Col span={14} className="public-invoice">
+                <Col span={8} className="public-invoice">
                    <PublicInvoice />
                 </Col>
-                <Col span={8} className="personal-invoice">
+                <Col span={12} className="personal-invoice">
                     <PersonalInvoice />
                 </Col>
             </Row>  
@@ -32,13 +32,17 @@ class Dashboard extends React.Component {
 
 const PublicInvoice = () =>{
    const [loading, setLoading] = useState(false);
-   const [personalInvoice, setPersonalInvoice] = useState([]);
+   const [publicInvoice, setPublicInvoice] = useState([]);
    const [countPublic, setCountPublic] = useState(0);
 
+   const setUnreadNum = async (type, setNum) => {
+    const resp = await getUnreadNum(type);
+    setNum(resp.num);
+   }
     useEffect(() => {
         loadData();
-        setCountPublic(getUnreadNum("public").num);
-    });
+        setUnreadNum("PUBLIC", setCountPublic);
+    },  []);
 
     const clearNum = () => {
         setCountPublic(0);
@@ -46,19 +50,15 @@ const PublicInvoice = () =>{
     }
 
     const loadData = async () => {
-        if (loading) {
-            return;
-        }
         setLoading(true);
-
         try {
             const resp = await getPublicInvoice();
-                setPersonalInvoice(resp)
+            setPublicInvoice(oldData => [...resp]);
             } catch (error) {
                 message.error(error.message);
             } finally {
                 setLoading(false);
-        }
+            }
     };
     
     return (
@@ -66,7 +66,6 @@ const PublicInvoice = () =>{
             <div
                 id="public-invoice"
                 style={{
-                  height: 700,
                   overflow: 'auto',
                   padding: '0 16px',
                   border: '1px solid rgba(140, 140, 140, 0.35)',
@@ -79,12 +78,16 @@ const PublicInvoice = () =>{
                 <Badge count={countPublic} />
                 <List
                     loading={loading}
-                    dataSource={personalInvoice}
+                    dataSource={publicInvoice}
                     renderItem={(item) => (
                         <List.Item>
                             <List.Item.Meta
                             description={
-                                <Text>{item.text}</Text>
+                                <div>
+                            <Text>{item.text}</Text>
+                            <br/>
+                            <Text>{item.date}</Text>
+                            </div>
                             }
                             />
                         </List.Item>
@@ -100,7 +103,11 @@ const PersonalInvoice = () => {
   const [visitPayment, setVisitPayment] = useState(false);
   const [visitMail, setVisitMail] = useState(false);
   const [visitOther, setVisitOther] = useState(false);
-  const [countReservation, setCountReservation] = useState(10);
+  const [mailLoading, setMailLoading] = useState(false);
+  const [otherLoading, setOtherLoading] = useState(false);
+  const [paymentLoading, setPaymentLoading] = useState(false);
+  const [reservationLoading, setReservationLoading] = useState(false);
+  const [countReservation, setCountReservation] = useState(0);
   const [countPayment, setCountPayment] = useState(0);
   const [countMail, setCountMail] = useState(0);
   const [countOther, setCountOther] = useState(0);
@@ -108,41 +115,44 @@ const PersonalInvoice = () => {
   const [mailList, setMailList] = useState([]);
   const [paymentList, setPaymentList] = useState([]);
   const [otherList, setOtherList] = useState([]);
-
+  const setUnreadNum = async (type, setNum) => {
+    const resp = await getUnreadNum(type);
+    setNum(resp.num);
+   }
   useEffect(() => {
-        setCountMail(getUnreadNum("mail").num);
-        setCountReservation(getUnreadNum("reservation").num);
-        setCountOther(getUnreadNum("other").num);
-        setCountPayment(getUnreadNum("payment").num);
+        setUnreadNum("MAIL", setCountMail);
+        setUnreadNum("OTHER", setCountOther);
+        setUnreadNum("RESERVATION", setCountReservation);
+        setUnreadNum("PAYMENT", setCountPayment);
         checkDue();
-    });
-    
+    }, []);
+
   const clickMailDrawer = () => {
     setVisitMail(true);
     setCountMail(0);
-    clearPersonalInvoice("mail");
-    loadMailData();
+    clearPersonalInvoice("MAIL");
+    loadData("MAIL", setMailList, setMailLoading);
   };
 
   const clickOtherDrawer = () => {
     setVisitOther(true);
     setCountOther(0);
-    clearPersonalInvoice("other");
-    loadOtherData();
+    clearPersonalInvoice("OTHER");
+    loadData("OTHER", setOtherList, setOtherLoading);
   };
 
   const clickReservationDrawer = () => {
     setVisitReservation(true);
     setCountReservation(0);
-    clearPersonalInvoice("reservation");
-    loadReservarionData();
+    clearPersonalInvoice("RESERVATION");
+    loadData("RESERVATION", setReservationList, setReservationLoading);
   };
 
   const clickPaymentDrawer = () => {
     setVisitPayment(true);
     setCountPayment(0);
-    clearPersonalInvoice("payment");
-    loadPaymentData();
+    clearPersonalInvoice("PAYMENT");
+    loadData("PAYMENT", setPaymentList, setPaymentLoading);
   };
 
   const onReservationDrawerClose = () => {
@@ -161,171 +171,98 @@ const PersonalInvoice = () => {
     setVisitOther(false);
   };
 
-  const loadReservarionData = async () => {
+  const loadData = async (type, setList, setLoading) => {
+    setLoading(true)
     try {
-            const resp = await getPersonalInvoice("reservation");
-            setReservationList(resp)
-        } catch (error) {
-            message.error(error.message);
-        } 
-    };
-
-  const loadMailData = async () => {
-    try {
-            const resp = await getPersonalInvoice("mail");
-            setMailList(resp)
-        } catch (error) {
-            message.error(error.message);
-        } 
-  };
-
-  const loadPaymentData = async () => {
-    try {
-         const resp = await getPersonalInvoice("payment");
-         setPaymentList(resp)
-        } catch (error) {
-            message.error(error.message);
-        } 
-  };
-
-  const loadOtherData = async () => {
-
-    try {
-            const resp = await getPersonalInvoice("other");
-            setOtherList(resp)
-        } catch (error) {
-            message.error(error.message);
-        } 
-  };
+        const resp = await getPersonalInvoice(type);
+        setList(resp)
+    } catch (error) {
+        message.error(error.message);
+    } finally{
+        setLoading(false)
+    }
+  }
 
     return (
-        <>
+        <Row className="personal_invoice">
+        <Col>
           <Space
             align="center"
             direction="vertical"
           >
             <div>
-                <Button type="primary" onClick={clickMailDrawer}>
+                <Button 
+                type="primary" 
+                onClick={clickMailDrawer}
+                loading={mailLoading}>
                 Mail
                 </Button>
                 <Badge count={countMail} />
             </div>
             <div>
-                <Button type="primary" onClick={clickOtherDrawer}>
+                <Button 
+                type="primary" 
+                onClick={clickOtherDrawer}
+                loading={otherLoading}>
                 Other
                 </Button>
                 <Badge count={countOther}/>
             </div>
             <div>
-                <Button type="primary" onClick={clickReservationDrawer}>
+                <Button 
+                type="primary" 
+                onClick={clickReservationDrawer}
+                loading={reservationLoading}>
                 Reservation 
                 </Button>
                 <Badge count={countReservation}/>
             </div>
             <div>
-                <Button type="primary" onClick={clickPaymentDrawer}>
+                <Button 
+                type="primary" 
+                onClick={clickPaymentDrawer}
+                loading={paymentLoading}>
                 Payment
                 </Button>
                 <Badge count={countPayment}/>
             </div>
           </Space>
-
-          <Drawer
-            title={"Reservation"}
-            placement="right"
-            width={750}
-            visible={visitReservation}
-            onClose={onReservationDrawerClose}
-            extra={
-                <>
-                  <Button onClick={onReservationDrawerClose}>Close</Button>
-                </>
-              }
-          >
-            <List
-                dataSource={reservationList}
-                renderItem={(item) => (
-                    <List.Item>
-                        <List.Item.Meta
-                        description={
-                            <Text>{item.text}</Text>
-                        }
-                        />
-                    </List.Item>
-                )}
-            />
-          </Drawer>
-          <Drawer
-            title={"Payment"}
-            placement="right"
-            width={750}
-            visible={visitPayment}
-            onClose={onPaymentDrawerClose}
-            extra={
-                  <Button onClick={onPaymentDrawerClose}>Close</Button>
-              }
-          >
-            <List
-                dataSource={paymentList}
-                renderItem={(item) => (
-                    <List.Item>
-                        <List.Item.Meta
-                        description={
-                            <Text>{item.text}</Text>
-                        }
-                        />
-                    </List.Item>
-                )}
-            />
-          </Drawer>
-          <Drawer
-            title={"Mail"}
-            placement="right"
-            width={750}
-            visible={visitMail}
-            onClose={onMailDrawerClose}
-            extra={
-                  <Button onClick={onMailDrawerClose}>Close</Button>
-              }
-          >
-            <List
-                dataSource={mailList}
-                renderItem={(item) => (
-                    <List.Item>
-                        <List.Item.Meta
-                        description={
-                            <Text>{item.text}</Text>
-                        }
-                        />
-                    </List.Item>
-                )}
-            />
-          </Drawer>
-          <Drawer
-            title={"Other"}
-            placement="right"
-            width={750}
-            visible={visitOther}
-            onClose={onOtherDrawerClose}
-            extra={
-                  <Button onClick={onOtherDrawerClose}>Close</Button>
-              }
-          >
-            <List
-                dataSource={otherList}
-                renderItem={(item) => (
-                    <List.Item>
-                        <List.Item.Meta
-                        description={
-                            <Text>{item.text}</Text>
-                        }
-                        />
-                    </List.Item>
-                )}
-            />
-          </Drawer>
-        </>
+          </Col>
+          {visitMail && <PersonalInvoiceDrawer onClose={onMailDrawerClose} type="MAIL" invoiceList={mailList} visible={visitMail}/>}
+          {visitOther && <PersonalInvoiceDrawer onClose={onOtherDrawerClose} type="OTHER" invoiceList={otherList} visible={visitOther}/>}
+          {visitPayment && <PersonalInvoiceDrawer onClose={onPaymentDrawerClose} type="PAYMENT" invoiceList={paymentList} visible={visitPayment}/>}
+          {visitReservation && <PersonalInvoiceDrawer onClose={onReservationDrawerClose} type="RESERVATION" invoiceList={reservationList} visible={visitReservation}/>}
+        </Row>
       )
 };
+
+const PersonalInvoiceDrawer = ({onClose, type, invoiceList, visible}) => {
+    return (
+        <Drawer
+            title={type}
+            placement="right"
+            width={750}
+            visible={visible}
+            onClose={onClose}
+          >
+            <List
+                dataSource={invoiceList}
+                renderItem={(item) => (
+                    <List.Item>
+                        <List.Item.Meta
+                        description={
+                            <div>
+                            <Text>{item.text}</Text>
+                            <br/>
+                            <Text>{item.date}</Text>
+                            </div>
+                        }
+                        />
+                    </List.Item>
+                )}
+            />
+          </Drawer>
+    )
+}
 
 export default Dashboard;
